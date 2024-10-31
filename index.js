ad2d7ed24aec8a7e4da5d5ac1ac432e2adf815ce
Bart <brayson@live.com>
2024-10-31 12:45:55 +0000

    added associations

--- index.js
+++ index.js
@@ -3,7 +3,11 @@ const { Musician } = require('./models/Musician')
 const { Song } = require("./models/Song")
 // Define associations here
 
+Band.hasMany(Musician);
+Musician.belongsTo(Band)
 
+Band.hasMany(Song);
+Song.hasMany(Band)
 
 module.exports = {
     Band,
